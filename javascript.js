dc2a7f0ff372bac9d113f9ab88ba44419f5a1ec5
Sean Markham <sean@seanmarkham.com>
2025-03-24 11:41:45 -0600

    Framework for JS code

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//First, dynamically generate DOM
+
+//set a function or variable to modify calc window
+//function to take input from button to push it to window
+
+
+//Then implement math functionality within calculator window
+//add fnct
+//subtract fnc
+//multiply func
+//divite func
+//operate fnc- take operator and 2 vars and call above func
+ 
+
+//implement nice-to-haves in dom 
+//like clearScreen();
+
+
+//SPECS
+//must ONLY work on TWO (2) numbers and ONE (1) operator
+//Round answers 
+//Error handling if operate() is called [equal sign pressed] before input complete
+//eraseAll() to kill all data in input
+//backspace() to backspace 
+//needs funky divideBy0 message
+//Apparently double-pressing the operator is bad
+//Add decimal input
+//After results are displayed, need to start new calculation
+
