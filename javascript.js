b62f88580cc69adbdb5f1c13d4e1e413d5710687
Sean Markham <sean@seanmarkham.com>
2025-03-31 08:29:45 -0600

    Implement window functionality & unused clearWindow()

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //First, dynamically generate DOM
 const buttonTray = document.querySelector('.button-tray');
-const calcWindow = document.querySelector('.calculator-windwow');
+const calcWindow = document.querySelector('.calculator-window');
 const buttons = document.querySelectorAll('.button');
 //set a function or variable to modify calc window
 function getWindow() {
@@ -13,14 +13,31 @@ function getButtonText() {
 };
 //and add it to each button
 for (each of buttons) {
-	each.addEventListener('click', getButtonText);
-}
+	each.addEventListener('click', addToWindow);
+	
+};
 
 //function to take input from button to push it to window
-
+function addToWindow() {
+	const value = this.textContent;
+	temp = calcWindow.textContent + value;
+	calcWindow.textContent = temp;
+	//in order to concatenate the string,
+	//because of calcWindow only returning a DOM node,
+	//we must do this weirdness
+	return value;
+}
+function clearWindow() {
+	calcWindow.textContent = '';
+}
 
 //Then implement math functionality within calculator window
 //add fnct
+function sum(a, b) {
+	return (a * 1) + (b * 1)
+	//do this goofiness to ensure that each is an int
+	//because you can't be too sure about things
+}
 //subtract fnc
 //multiply func
 //divite func
